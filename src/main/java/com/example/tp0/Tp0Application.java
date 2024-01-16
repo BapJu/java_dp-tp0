2fa3b384a827ded72a35e9fb7465823874f187fb
Baptiste JULIENNE <baptiste.julienne@gmail.com>
2024-01-16 09:25:42 +0100

    init

---
+++ src/main/java/com/example/tp0/Tp0Application.java
@@ -0,0 +1,13 @@
+package com.example.tp0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp0Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tp0/Tp0ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tp0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp0ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
